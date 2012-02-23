b93d23306645bc6ac2d1c693bfa80298b990c9ee
Pieter Loubser <pieter.loubser@puppetlabs.com>
2012-02-23 19:01:44 +0000

    First attempt at mco plugin application with package action

--- lib/mcollective.rb
+++ lib/mcollective.rb
@@ -56,6 +56,7 @@ module MCollective
   autoload :Applications, "mcollective/applications"
   autoload :Vendor, "mcollective/vendor"
   autoload :Shell, "mcollective/shell"
+  autoload :Plugins, "mcollective/plugin"
 
   MCollective::Vendor.load_vendored
 
